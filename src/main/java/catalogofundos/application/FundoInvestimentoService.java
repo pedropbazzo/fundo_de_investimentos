958a905edabec7bd3d16399ed1b665ff89648af6
“pedropbazzo” <you@example.com>
2021-12-27 23:45:07 -0300

    Refactor(#service) : FundoInvestimentoService

---
+++ src/main/java/catalogofundos/application/FundoInvestimentoService.java
@@ -0,0 +1,10 @@
+package catalogofundos.application;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FundoInvestimentoService {
+
+	public FundoInvestimento adicionar(FundoInvestimento fundo);
+	
+}

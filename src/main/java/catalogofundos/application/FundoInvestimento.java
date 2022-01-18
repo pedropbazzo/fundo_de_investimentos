ee0fa9810d50cc2e8a2441cbaf4809157b34b79d
“pedropbazzo” <you@example.com>
2022-01-18 02:18:08 -0300

    enum

--- src/main/java/catalogofundos/application/FundoInvestimento.java
+++ src/main/java/catalogofundos/application/FundoInvestimento.java
@@ -33,62 +33,6 @@ public class FundoInvestimento {
 		this.gestor = gestor;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getAplicacaoMinima() {
-		return aplicacaoMinima;
-	}
-
-	public void setAplicacaoMinima(BigDecimal aplicacaoMinima) {
-		this.aplicacaoMinima = aplicacaoMinima;
-	}
-
-	public Risco getRisco() {
-		return risco;
-	}
-
-	public void setRisco(Risco risco) {
-		this.risco = risco;
-	}
-
-	public Categoria getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
-
-	public Double getTaxaAdministracao() {
-		return taxaAdministracao;
-	}
-
-	public void setTaxaAdministracao(Double taxaAdministracao) {
-		this.taxaAdministracao = taxaAdministracao;
-	}
-
-	public String getGestor() {
-		return gestor;
-	}
-
-	public void setGestor(String gestor) {
-		this.gestor = gestor;
-	}
-
 	@Override
 	public String toString() {
 		return "Fundo [id=" + id + ", nome=" + nome + ", aplicacaoMinima=" + aplicacaoMinima + ", risco=" + risco
